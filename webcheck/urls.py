25ab0266951be7d84f260c105054ce3beb2bdd7f
3112shubham <123260167+3112shubham@users.noreply.github.com>
2025-09-02 18:18:11 +0530

    Update urls.py

--- webcheck/urls.py
+++ webcheck/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings 
-SCRIPT_PREFIX = getattr(settings, 'FORCE_SCRIPT_NAME', None) or ''
+# SCRIPT_PREFIX = getattr(settings, 'FORCE_SCRIPT_NAME', None) or ''
+# urlpatterns = [
+#     path(f'{SCRIPT_PREFIX.lstrip("/")}/admin/' if SCRIPT_PREFIX else 'admin/', admin.site.urls),
+#     path(f'{SCRIPT_PREFIX.lstrip("/")}/' if SCRIPT_PREFIX else '', include('webapp.urls')),
+# ]
 urlpatterns = [
-    path(f'{SCRIPT_PREFIX.lstrip("/")}/admin/' if SCRIPT_PREFIX else 'admin/', admin.site.urls),
-    path(f'{SCRIPT_PREFIX.lstrip("/")}/' if SCRIPT_PREFIX else '', include('webapp.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('webapp.urls')),
 ]
